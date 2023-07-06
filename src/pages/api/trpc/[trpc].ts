1604f833d6fdaafee648adba693457408509b3a3
icep0ps <tapsmuko@gmail.com>
2023-07-06 14:18:53 +0200

    fix: cache to vercel cdn

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -2,6 +2,10 @@ import { createContext } from '../../../server/context';
 import { appRouter } from '../../../server/routes/_app';
 import { createNextApiHandler } from '@trpc/server/adapters/next';
 
+export const config = {
+  runtime: 'nodejs',
+};
+
 export default createNextApiHandler({
   router: appRouter,
   createContext,
@@ -12,9 +16,6 @@ export default createNextApiHandler({
     return {
       headers: {
         'Vercel-CDN-Cache-Control': 'public, s-max-age=3699, stale-while-revalidate=100',
-        'CDN-Cache-Control': 'max-age=60',
-        'Cache-Control': 'max-age=10',
-        'X-custom': 'sent headers',
       },
     };
   },
